40a80f421ad47c5cf157f085b8c7cb0b5ee1387a
Ben Lander <ben@benlander.me>
2020-01-23 21:40:54 +0000

    Initial commit

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ben Lander on 23/01/2020.
+//  Copyright © 2020 Ben Lander. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
